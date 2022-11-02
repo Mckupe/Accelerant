4a82eeb9ff4c3ae0b68c753a38b4c5dfb9231cf8
Mckupe <nohrin09072001@yandex.ru>
2022-11-02 07:52:45 +0500

    добавил vk api2

---
+++ routes/vkRoute.js
@@ -0,0 +1,14 @@
+const Router = require('express');
+const vkContriller = require('../controllers/vkController');
+const router = new Router();
+const authMiddleware = require('../middlewares/auth-middleware');
+
+router.post('/post', authMiddleware, vkContriller.post); // добавление поста
+router.post('/message', authMiddleware, vkContriller.message); // отправка сообщений
+//router.get('/getAll', authMiddleware, vkContriller.getPosts); // получение всех постов для конкретного проекта
+//router.get('/getOne', authMiddleware, vkContriller.getOnePost); // получение отдельного поста
+//router.put('/update', authMiddleware, vkContriller.updatePost); // обновление поста
+//router.delete('/delete', authMiddleware, vkContriller.deletePost); // удаление поста
+
+
+module.exports = router;
